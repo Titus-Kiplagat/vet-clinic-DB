673012b18b68115326ab0bfd7550b1f42f421a3d
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-10-27 18:59:22 +0300

    Add data to both owners and species table

--- data.sql
+++ data.sql
@@ -16,3 +16,17 @@ VALUES
 ('Boarmon', date '2005-06-07', 7, true, 20.4),
 ('Blossom', date '1998-10-13', 3, true, 17),
 ('Ditto', date '2022-05-14', 4, true, 22);
+
+INSERT INTO owners (full_name, age) 
+VALUES 
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bod', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
